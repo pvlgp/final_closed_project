472eabc3cdcf6dd5f1b220c9aece1589faa59494
Pavel <p3543815@icloud.com>
2025-09-09 16:41:01 +0300

    Реализована основная функция, выполняющая ETL процесс.

--- producer_from_mongodb.py
+++ producer_from_mongodb.py
@@ -31,8 +31,31 @@ logger = logging.getLogger("producer_from_mongodb")
 # получаем параметры с конфиденциальной информацией
 param_conf = {
     "client": os.getenv("CLIENT_MONGO"),
-    "db": os.getenv("DB_NAME")
+    "db": os.getenv("DB_NAME"),
+    "Kafka_server": os.getenv("KAFKA_BOOTSTRAP_SERVERS")
 }
+
+def get_list_collections(url: str, db_name) -> list:
+    """
+    Функция получает список коллекций в указанной базе данных
+    :param url: URL для подключения к серверу MongoDB
+    :param db_name: Имя базы данных, к которой нужно подключиться
+    :return: объект list со списком коллекций
+    """
+    try:
+        logger.info(f"Попытка подключения к БД {db_name}")
+        # подключаемся к базе данных
+        with MongoClient(url, serverSelectionTimeoutMS=5000) as client:
+            db = client[db_name]
+            logger.info(f"Попытка получения списка коллекций в БД {db_name}")
+            # получаем список коллекций
+            collections_names = db.list_collection_names()
+            logger.info("Список коллекций успешно получен")
+            return collections_names
+    except Exception as mon_err:
+        logger.error(f"Ошибка при работе с MongoDB: {mon_err}")
+        # при ошибке возвращаем пустой список
+        return []
 
 def get_data_from_mongodb(url: str, db_name: str, collect_name: str) -> list:
     """
@@ -105,4 +128,48 @@ def send_to_kafka(producer: KafkaProducer, topic: str, data: list) -> None:
         logger.error(f"Ошибка при отправке в Kafka {err}")
 
 def main() -> None:
-    pass
+    """
+    Основная функция выполняющая ETL процесс отправки данных в Kafka.
+    Extract - из MongoDB, Load - в Kafka.
+    :return: None
+    """
+    try:
+        logger.info("Запуск Producer для загрузки данных из MongoDB в Kafka")
+        # получаем список коллекций в указанной БД
+        list_collections = get_list_collections(param_conf["client"], param_conf["db"])
+        logger.info(f"Найдены коллекции: {", ".join(list_collections)}")
+        # создаем Kafka Producer
+        logger.info("Создание Kafka Producer")
+        producer = KafkaProducer(
+            bootstrap_servers=param_conf["Kafka_server"],
+            acks="all",
+            retries=3
+        )
+        # проходим по списку с коллекциями
+        for collection_name in list_collections:
+            logger.info(f"Обработка коллекции: {collection_name}")
+            # получаем данные из коллекции
+            data = get_data_from_mongodb(
+                url=param_conf["client"],
+                db_name=param_conf["db"],
+                collect_name=collection_name
+            )
+            # определяем имя топика
+            kafka_topik = f"mongodb_{param_conf["db"]}_{collection_name}"
+            # отправляем данные в топик
+            logger.info(f"Отправка {len(data)} документов в топик {kafka_topik}")
+            send_to_kafka(
+                producer=producer,
+                topic=kafka_topik,
+                data=data
+            )
+            logger.info(f"Коллекция {collection_name} обработана")
+        producer.close()
+        logger.info("ETL процесс завершен")
+    except Exception as main_err:
+        logger.error(f"Ошибка в функции main: {main_err}")
+    finally:
+        logger.info("Завершение работы Producer")
+
+if __name__ == "__main__":
+    main()
